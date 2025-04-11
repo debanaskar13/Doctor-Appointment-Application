3583949d092ce16fab692b6526b78a01d322b6b7
debanaskar13 <debanaskar13@gmail.com>
2025-04-11 17:07:24 +0530

    Added Mobile Responsive Menu

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -10,6 +10,8 @@ const Doctors = () => {
 
   const {speciality} = useParams()
   const [filterDoc,setFilterDoc] = useState([])
+  const [showFilter,setShowFilter] = useState(false)
+
   const navigate = useNavigate()
 
   const {doctors} = useContext(AppContext)
@@ -43,7 +45,8 @@ const Doctors = () => {
     <div>
         <p className='text-gray-600'>Browse through the doctors specialist.</p>
         <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
-          <div className=' flex flex-col gap-4 text-sm text-gray-600'>
+          <button className={`py-1 px-3 border border-gray-300 rounded text-sm transition-all sm:hidden ${showFilter ? 'bg-primary text-white' : ''}`} onClick={() => setShowFilter(prev => !prev)}>Filter</button>
+          <div className={`flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex' : 'hidden sm:flex'}`}>
 
             {
               specialityList.map((special,index) => (
