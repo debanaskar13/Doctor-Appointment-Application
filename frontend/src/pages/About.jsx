ee2f4d0d3d08046d3ab44b52b964819ff2415ae7
debanaskar13 <debanaskar13@gmail.com>
2025-04-11 14:53:27 +0530

    Designed Loing ,Contact US and About Us Page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,10 +1,50 @@
 
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>ABOUT <span className='text-gray-700 font-medium'>US</span></p>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-12'>
+        <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
+          <p>Welcome To RxFlow . Your Trusted Partner in Managing your Healthcare Needs Conveniently and Efficiently. At RxFlow , We Understand the Challenges Individual Face When It Comes To Scheduling Doctor Appointment And Managing Their Health Records</p>
+          <p>RxFlow is Commited to Execute in Healthcare Technology. We Continuously Strive To Enhance Our Platform . Integrating The Latest Advancements To Improve User Experience and Deliver Superior Service. Whether You&apos;re Booking Your First Appintment Or Managing Ongoing Care. RxFlow is here To Support You Evry Step Of The Way.    </p>
+          <b className='text-gray-800'>Our Vision</b>
+          <p>Our Vision At RxFlow is To Create a Seamless Healthcare Experience For  Every User. We Aim To Bridge The Gap Between Patients And Helthcare Providers, Making It Easier For You, To Access The Care You Need When You Need It.   </p>
+        </div>
+      </div>
+
+      <div className='text-xl my-4'>
+        <p>WHY <span className='text-gray-700 font-semibold'>CHOOSE US</span></p>
+      </div>
+
+      <div className='flex flex-col md:flex-row mb-20'>
+        <div className='border border-gray-300 px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-[#5F6FFF] hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>EFFICIENCY</b>
+          <p>Streamlined appointment scheduling thats fits into your busy lifestyle</p>
+        </div>
+
+
+        <div className='border border-gray-300 px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-[#5F6FFF] hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>CONVENIENCE</b>
+          <p>Access to a network of trusted helthcare professionals in your area.</p>
+        </div>
+
+
+        <div className='border border-gray-300 px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-[#5F6FFF] hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>PERSONALIZATION</b>
+          <p>Tailored recommendations and reminders to help you stay on top of your health.</p>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,54 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [state, setState] = useState("Sign Up")
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [name, setName] = useState("")
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault()
+
+
+  }
+
+
   return (
-    <div>Login</div>
+    <form className='min-h-[80vh] flex items-center' onSubmit={onSubmitHandler}>
+      <div className='flex flex-col m-auto gap-3 items-center p-8 min-w-[340px] sm:min-w-96 border border-gray-300 rounded-xl text-zinc-600 text-sm shadow-lg'>
+        <p className='text-2xl font-semibold'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</p>
+        <p>Please {state === 'Sign Up' ? 'sign up' : 'log in'} to book appointment</p>
+        {
+          state === 'Sign Up'
+            ? <div className='w-full'>
+              <p>Full Name</p>
+              <input className='border border-zinc-300 rounded-xl w-full p-2 mt-1' type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+            </div>
+            : ''
+        }
+        <div className='w-full'>
+          <p>Email</p>
+          <input className='border border-zinc-300 rounded-xl w-full p-2 mt-1' type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        </div>
+        <div className='w-full'>
+          <p>Password</p>
+          <input className='border border-zinc-300 rounded-xl w-full p-2 mt-1' type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </div>
+
+        <button className='bg-primary text-white w-full py-2 rounded-md text-base'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</button>
+
+        {
+          state === 'Sign Up'
+            ? <p className=''>Already have an account? <span className='text-[#5F6FFF] underline cursor-pointer' onClick={() => setState('Login')}>Login here</span></p>
+            : <p>Create a new account? <span className='text-[#5F6FFF] underline cursor-pointer' onClick={() => setState('Sign Up')}>Click here</span></p>
+        }
+
+      </div>
+
+    </form>
   )
 }
 
